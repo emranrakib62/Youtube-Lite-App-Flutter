b5e58ba52b90dfeae223d71fed5d06fb4cee492e
Emranul Hoque <emranrakib01862@gmail.com>
2026-04-20 17:16:00 +0600

    updated

--- lib/Screen/Home/home_screen.dart
+++ lib/Screen/Home/home_screen.dart
@@ -16,25 +16,64 @@ class _HomeScreenState extends State<HomeScreen> {
       backgroundColor: AppColors.backgroundDark,
       appBar: AppBar(
         backgroundColor: AppColors.backgroundDark,
+        elevation: 0,
         title: Row(
           children: [
             Container(
+              width: 35,
+              height: 25,
               decoration: BoxDecoration(
                 color: AppColors.youtubePrimary,
                 borderRadius: BorderRadius.circular(6),
               ),
-              child: Icon(Icons.play_arrow, color: Colors.white),
+              child: const Center(
+                child: Icon(Icons.play_arrow, color: Colors.white),
+              ),
             ),
-            SizedBox(width: 8), // spacing
+            const SizedBox(width: 8),
             Text(
               AppStrings.appName,
-              style: TextStyle(
+              style: const TextStyle(
                 color: Colors.white,
                 fontSize: 18,
                 fontWeight: FontWeight.bold,
               ),
             ),
           ],
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.search, color: Colors.white),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.notifications, color: Colors.white),
+          ),
+        ],
+      ),
+
+      // BODY
+      body: ListView(
+        children: [
+          _buildSectionHeader(AppStrings.sectionBrowse),
+          _buildSectionHeader(AppStrings.sectionLibrary),
+          _buildSectionHeader(AppStrings.sectionMore),
+        ],
+      ),
+    );
+  }
+
+  // SECTION HEADER WIDGET
+  Widget _buildSectionHeader(String title) {
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: Text(
+        title,
+        style: TextStyle(
+          color: AppColors.textHint,
+          fontSize: 12,
+          fontWeight: FontWeight.w600,
         ),
       ),
     );
