b637abcd2fabb41f7dd2487e6160c58c0cd4236c
Emranul Hoque <emranrakib01862@gmail.com>
2026-04-20 16:52:57 +0600

    d_2

--- lib/Screen/Home/home_screen.dart
+++ lib/Screen/Home/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:youtube_liteee/core/constants/app_color.dart';
+import 'package:youtube_liteee/core/constants/app_string.dart';
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -16,7 +18,22 @@ class _HomeScreenState extends State<HomeScreen> {
         backgroundColor: AppColors.backgroundDark,
         title: Row(
           children: [
-            Icon(Icons.play_arrow,color:Colors.white)
+            Container(
+              decoration: BoxDecoration(
+                color: AppColors.youtubePrimary,
+                borderRadius: BorderRadius.circular(6),
+              ),
+              child: Icon(Icons.play_arrow, color: Colors.white),
+            ),
+            SizedBox(width: 8), // spacing
+            Text(
+              AppStrings.appName,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
           ],
         ),
       ),
